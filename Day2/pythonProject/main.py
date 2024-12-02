34f3be2e6bc5bfa8c6894d534865dbd52eac675e
Rybacki <adels123456@gmail.com>
2024-12-03 00:04:16 +0100

    solve day 2 but i took solution from internet

--- Day2/pythonProject/main.py
+++ Day2/pythonProject/main.py
@@ -1,23 +1,32 @@
-with open("data_example") as file:
-    safe = 0
+def check(tab):
+    positive = [1,2,3]
+    negative = [-1, -2, -3]
+    differences = []
+    pos = True
+    neg = True
+    for i in range(len(tab) - 1):
+        difference = tab[i+1] - tab[i]
+        differences.append(difference)
+    for i in differences:
+        if i not in positive:
+            pos = False
+    for i in differences:
+        if i not in negative:
+            neg = False
+    if pos or neg:
+        return True
+    return False
+
+safe = 0
+with open("data") as file:
     for line in file:
-        data = line.split()
-        data = list(map(int, data))
-        skip = False
-        desc = True
-        asc = True
-        temp = data
-        counter = 0
-        for i in range(len(temp)-1):
-            if temp[i] > temp[i+1]:
-                continue
-            else:
-                if counter == 0:
-                    counter += 1
-                    continue
-                else:
-                    desc = False
-                    break
-        print(desc)
-        exit()
-print(safe)
+        row = [int(i) for i in line.split()]
+        if check(row):
+            safe += 1
+            continue
+        for i in range(len(row)):
+            temp = row[:i] + row[i + 1:]
+            if check(temp):
+                safe += 1
+                break
+    print(safe)
