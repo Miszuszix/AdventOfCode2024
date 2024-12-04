07a8c804e082fc855d15c5c206f2923996c0bff9
Rybacki <adels123456@gmail.com>
2024-12-04 18:47:27 +0100

    solve part 2

--- Day4/main.py
+++ Day4/main.py
@@ -30,15 +30,17 @@ def reverse(tab1):
         tab1[i] = tab1[i][::-1]
     return tab1
 
+def countMAS(tab1):
+    global xmas
+    for i in range(len(tab1) - 2):
+        for j in range(len(tab1[i]) - 2):
+            if tab1[i][j] == "M" and tab1[i + 1][j + 1] == "A" and tab1[i + 2][j + 2] == "S" or tab1[i][j] == "S" and tab1[i + 1][j + 1] == "A" and tab1[i + 2][j + 2] == "M":
+                if tab[i + 2][j] == "M" and tab[i + 1][j + 1] == "A" and tab[i][j + 2] == "S" or tab[i + 2][j] == "S" and tab[i + 1][j + 1] == "A" and tab[i][j + 2] == "M":
+                    xmas += 1
+
 with open("data") as file:
     for line in file:
         line = line.strip()
         tab.append(line)
-countHorizontal(tab)
-tab = transponate(tab)
-countHorizontal(tab)
-tab = transponate(tab)
-countDiagonal(tab)
-tab = reverse(tab)
-countDiagonal(tab)
+countMAS(tab)
 print(xmas)
